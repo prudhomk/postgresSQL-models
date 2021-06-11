cbcc5126f326ce8cdf1a9a230e1ed45135663873
Kalan Prudhomme <cleaver9@gmail.com>
2021-06-11 08:26:04 -0700

    added findall games route, test pass

--- __tests__/game.test.js
+++ __tests__/game.test.js
@@ -22,4 +22,31 @@ describe('game routes', () => {
       console: 'Nintendo 64'
     });
   });
+
+  it('finds all games via GET', async () => {
+
+    const mario = await Game.insert({
+      title: 'Super Mario 64',
+      rating: 'E',
+      price: 59.99,
+      console: 'Nintendo 64'
+    });
+
+    const zelda = await Game.insert({
+      title: 'Legend of Zelda',
+      rating: 'E',
+      price: 59.99,
+      console: 'Nintendo Entertainment System'
+    });
+
+    const huntdown = await Game.insert({
+      title: 'Huntdown',
+      rating: 'T',
+      price: 19.99,
+      console: 'All'
+    });
+
+    const res = await request(app).get('/api/v1/games');
+    expect(res.body).toEqual([mario, zelda, huntdown]);
+  });
 });
